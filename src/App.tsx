64ffa12e2c3c945d1f3cf9e3aaf5908169228437
Shimon Dotera <dotera.s@tam-tam.co.jp>
2025-01-15 20:46:49 +0900

    remove: 不要記述

--- src/App.tsx
+++ src/App.tsx
@@ -22,25 +22,12 @@ const LevelText = styled.div`
   position: relative;
 `;
 
-const BurstEffect = styled.div<{ visible: boolean }>`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  z-index: 1;
-`;
-
 const App: React.FC = () => {
   const [activities, setActivities] = useState<
     { text: string; progress: number; timeStamp: Date }[]
   >([]);
   const [displayProgress, setDisplayProgress] = useState<number>(0);
   const [level, setLevel] = useState<number>(1);
-  // const [animate, setAnimate] = useState<boolean>(false);
-  const [burstVisible, setBurstVisible] = useState<boolean>(false);
 
   const getMaxProgress = (level: number): number => {
     return 4 + level; // レベル1で5、レベル2で6...
@@ -58,18 +45,8 @@ const App: React.FC = () => {
       setDisplayProgress(displayProgress + progress - maxProgress); // 表示上の進捗を調整（次のレベルに持ち越す）
       setLevel(level + 1); // レベルを1つ上げる
 
-      // アニメーションとconfettiをトリガー
-      // setAnimate(true);
-      setBurstVisible(true);
+      // 紙吹雪をトリガー
       triggerConfetti();
-
-      // setTimeout(() => {
-      //   setAnimate(false);
-      // }, 500);
-
-      setTimeout(() => {
-        setBurstVisible(false);
-      }, 1000);
     } else {
       // レベルアップしていないとき → いつも通り進捗を加算
       setDisplayProgress(displayProgress + progress);
@@ -100,7 +77,6 @@ const App: React.FC = () => {
     <div>
       <Header />
       <LevelContainer>
-        <BurstEffect visible={burstVisible} />
         <LevelText>Lv.{level}</LevelText>
       </LevelContainer>
       <ActivityInput onAddActivity={handleAddActivity} />
